b7a2c6af6fd5953804beea8be6e72d65f048fd94
dani <56475207+dimiskil@users.noreply.github.com>
2020-10-05 14:06:51 +0900

    Add files via upload

---
+++ precache-manifest.7370ca1d655f799df0dcee8ffbb79967.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df660a21e5e552ac6eb140ddc7d4285c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90cf51fad82b01145613",
+    "url": "/static/css/main.ff857de4.chunk.css"
+  },
+  {
+    "revision": "dc5c46eb92e3347356d5",
+    "url": "/static/js/2.d20a69ca.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d20a69ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90cf51fad82b01145613",
+    "url": "/static/js/main.b5ebf09e.chunk.js"
+  },
+  {
+    "revision": "86f2ec6c8b9569ec4b09",
+    "url": "/static/js/runtime-main.8fa676d4.js"
+  },
+  {
+    "revision": "9b0e5d1787454db5f34656874b155c05",
+    "url": "/static/media/Whitney-Black.9b0e5d17.woff"
+  },
+  {
+    "revision": "e8f34878595d20501035e9e74c132920",
+    "url": "/static/media/Whitney-Black.e8f34878.woff2"
+  },
+  {
+    "revision": "752a3bb681e31b1babc89b6ca1d7cb04",
+    "url": "/static/media/Whitney-Bold.752a3bb6.woff"
+  },
+  {
+    "revision": "7eb403beefb27b95ef4a6e71df682f12",
+    "url": "/static/media/Whitney-Bold.7eb403be.woff2"
+  },
+  {
+    "revision": "4539737d5c31eab13975c345abf8f9ba",
+    "url": "/static/media/Whitney-Light.4539737d.woff"
+  },
+  {
+    "revision": "baa7db9068e9472c3b6155ace6fbfc8e",
+    "url": "/static/media/Whitney-Light.baa7db90.woff2"
+  },
+  {
+    "revision": "805915b72a27c985687f89568a809db5",
+    "url": "/static/media/Whitney-Medium.805915b7.woff2"
+  },
+  {
+    "revision": "a37b6d9d61978081c039301f0a5e486f",
+    "url": "/static/media/Whitney-Medium.a37b6d9d.woff"
+  }
+]);
